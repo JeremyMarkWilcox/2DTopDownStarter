1960c4c392e3b7f28bf4fdbacde32e7f76fb2e50
Jeremy Mark Wilcox <jeremymarkwilcox@gmail.com>
2026-09-23 18:19:37 -0400

    Update PixelRoom.cs

--- Game/TopDown/PixelRoom.cs
+++ Game/TopDown/PixelRoom.cs
@@ -2,6 +2,7 @@ using Godot;
 
 public partial class PixelRoom : Node2D
 {
+    //Temp Art
     public override void _Draw()
     {
         DrawRect(new Rect2(0, 0, 960, 640), new Color("#243d40"));
